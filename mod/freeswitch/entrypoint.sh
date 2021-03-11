573046d84fdf44eed2385c1d5c07895853349725
capitalfuse <capitalfuse@gmail.com>
2021-03-11 11:19:21 +0900

    Modified freeswitch dockerfile and configuration xml files

--- mod/freeswitch/entrypoint.sh
+++ mod/freeswitch/entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # remove all SIP (port 5060) iptable rules
-iptables -S INPUT | grep "\-\-dport 5060 " | cut -d " " -f 2- | xargs -rL1 iptables -D
+#iptables -S INPUT | grep "\-\-dport 5060 " | cut -d " " -f 2- | xargs -rL1 iptables -D
 
 # block requests to 5060 (tcp/udp)
-iptables -A INPUT -p tcp --dport 5060 -s 0.0.0.0/0 -j REJECT
-iptables -A INPUT -p udp --dport 5060 -s 0.0.0.0/0 -j REJECT
+#iptables -A INPUT -p tcp --dport 5060 -s 0.0.0.0/0 -j REJECT
+#iptables -A INPUT -p udp --dport 5060 -s 0.0.0.0/0 -j REJECT
 
 # allow some IPs 
-IFS=',' read -ra ADDR <<< "$SIP_IP_ALLOWLIST"
-for IP in "${ADDR[@]}"; do
+#IFS=',' read -ra ADDR <<< "$SIP_IP_ALLOWLIST"
+#for IP in "${ADDR[@]}"; do
     # process "$i"
-    echo "allow port 5060/udp for $IP"
-    iptables -I INPUT  -p udp --dport 5060 -s $IP -j ACCEPT
-done
+#    echo "allow port 5060/udp for $IP"
+#    iptables -I INPUT  -p udp --dport 5060 -s $IP -j ACCEPT
+#done
 
 # create user 'freeswitch'
 # add it to group 'freeswitch'
@@ -28,7 +28,8 @@ chmod -R u=rwx,g=rx /usr/local/freeswitch/bin/*
 chown -R freeswitch:freeswitch /var/freeswitch/meetings
 chmod 777 /var/freeswitch/meetings
 
-dockerize \
-    -template /etc/freeswitch/vars.xml.tmpl:/etc/freeswitch/vars.xml \
-    -template /etc/freeswitch/autoload_configs/conference.conf.xml.tmpl:/etc/freeswitch/autoload_configs/conference.conf.xml \
-    /usr/local/freeswitch/bin/freeswitch -u freeswitch -g freeswitch -nonat -nf
+#dockerize \
+#    -template /etc/freeswitch/vars.xml.tmpl:/usr/local/freeswitch/conf/vars.xml \
+#    -template /etc/freeswitch/autoload_configs/conference.conf.xml.tmpl:/usr/local/freeswitch/conf/autoload_configs/conference.conf.xml \
+
+freeswitch -u freeswitch -g freeswitch -nonat -nf
